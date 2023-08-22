a1bd4387c39553eba57a2461a0372b37fb443040
Cameron Yee <makakoa57@gmail.com>
2023-08-22 13:12:52 -0700

    remove files, retry with gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 const path = require("path");
 
+const isDev = NODE_ENV === "development";
+
+const extended = isDev
+	? {
+			ode: "development",
+			devtool: "eval-cheap-source-map",
+			devServer: {
+				static: {
+					directory: path.join(__dirname, "dist"),
+				},
+				compress: true,
+				port: 9000,
+			},
+	  }
+	: {};
+
 module.exports = {
 	entry: {
 		hyptiotes: path.join(__dirname, "hyptiotes.js"),
@@ -9,13 +25,5 @@ module.exports = {
 		path: path.join(__dirname, "dist"),
 		filename: "[name].bundle.js",
 	},
-	mode: "development",
-	devtool: "eval-cheap-source-map",
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "dist"),
-		},
-		compress: true,
-		port: 9000,
-	},
+	...extended,
 };
